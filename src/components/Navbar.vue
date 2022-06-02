d3f4f704f1932d7e5d84107af740e27b1534570d
iconoyuri <iconoyuri@gmail.com>
2022-06-02 11:35:45 +0100

    Sessions handling : token storage and deletion via logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,17 +6,31 @@
                 <span class="app-name">MyTimetable</span>
             </span>
         </router-link>
-        <router-link v-if="notUserLogged" :to="{ name: 'login' }">
-            <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">
-                Log In
-            </button>
-        </router-link>
-        <router-link v-if="teacherLogged" :to="{ name: 'teacherworkshop' }">
-            <i class="fa-solid fa-chalkboard-user"></i>
-        </router-link>
-        <router-link v-if="adminLogged" :to="{ name: 'adminworkshop' }">
-            <i class="fa-solid fa-user-tie"></i>
-        </router-link>
+        <div class="options">
+            <router-link v-if="notUserLogged" :to="{ name: 'login' }">
+                <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">
+                    Log In
+                </button>
+            </router-link>
+            <div v-else class="user-options">
+                <router-link
+                    v-if="teacherLogged"
+                    :to="{ name: 'teacherworkshop' }"
+                >
+                    <i class="fa-solid fa-chalkboard-user"></i>
+                </router-link>
+                <router-link v-if="adminLogged" :to="{ name: 'adminworkshop' }">
+                    <i class="fa-solid fa-user-tie"></i>
+                </router-link>
+                <button
+                    class="btn btn-outline-danger logout"
+                    @click="logout"
+                    title="logout"
+                >
+                    <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                </button>
+            </div>
+        </div>
     </nav>
 </template>
 
@@ -25,6 +39,18 @@ export default {
     name: "Navbar",
     data() {
         return {};
+    },
+    methods: {
+        logout() {
+            console.log("Sending backend logout request");
+            this.axios
+                .delete("/logout", {
+                    params: { token: this.$store.getters.getAccessToken },
+                })
+                .then(() => {});
+            this.$store.commit("logout");
+            location.reload();
+        },
     },
     computed: {
         notUserLogged() {
@@ -49,5 +75,19 @@ nav {
     font-size: 2rem;
     font-weight: bold;
     font-family: Calibri;
+}
+.user-options {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+}
+.user-options .logout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 2rem;
+    height: 2rem;
+    font-size: 0.8rem;
+    flex-shrink: 0;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,29 @@ import { createStore } from "vuex";
 
 const store = createStore({
     state: {
-        access_token: "",
-        token_type: "",
-        user_type: "",
+        user_type: sessionStorage.getItem("user_type"),
+        access_token: sessionStorage.getItem("access_token"),
+        token_type: sessionStorage.getItem("token_type"),
         backend_domain: "https://time-table-app-g14.herokuapp.com",
     },
     mutations: {
         saveUser(state, { access_token, token_type, user }) {
-            state.access_token = access_token;
-            state.token_type = token_type;
-            state.user_type = user.toLowerCase();
+            sessionStorage.setItem("access_token", access_token);
+            sessionStorage.setItem("user_type", user.toLowerCase());
+            sessionStorage.setItem("token_type", token_type);
+
+            state.user_type = sessionStorage.getItem("user_type");
+            state.access_token = sessionStorage.getItem("access_token");
+            state.token_type = sessionStorage.getItem("token_type");
+        },
+        logout() {
+            sessionStorage.clear();
+            console.log("Logged out");
         },
     },
     getters: {
         isUserLogged(state, getters) {
-            return getters.userIsAdmin || getters.userIsTeacher
+            return getters.userIsAdmin || getters.userIsTeacher;
         },
         userIsAdmin(state) {
             return state.user_type == "administrateur";
